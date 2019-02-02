67453fb22ae2f682ce2b1206b6caa29ef2471c80
EC2 Default User <ec2-user@ip-172-31-4-126.us-east-2.compute.internal>
2019-02-02 14:06:46 +0000

    Generate User using scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :users
   root 'shiritori_messages#index'
   get 'shiritori_messages/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
